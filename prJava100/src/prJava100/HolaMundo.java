081db30c0a8bda9b15de9e2b8b0ca8ec6ec786d8
1DAW-00 <1DAW-00@PC04>
2019-03-22 09:12:52 +0100

    Modifico clase hola mundo

--- prJava100/src/prJava100/HolaMundo.java
+++ prJava100/src/prJava100/HolaMundo.java
@@ -5,7 +5,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Adios");
+		System.out.println("Modifico esta clase");
+		System.out.println("Añado esto desde la rama1");
 	}
 
 }
